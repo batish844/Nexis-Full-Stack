e0c1cdb7dcc45b79865f9774ed23ef1cd24d1de3
ahmadsharara39 <ahmadsharara03@gmail.com>
2024-11-23 01:31:42 +0200

    Created CartController, didnt work on it

--- routes/web.php
+++ routes/web.php
@@ -89,3 +89,4 @@ Route::get('/filter/men', [MenController::class, 'filterProducts'])->name('men.f
 Route::get('/filter/women', [WomenController::class, 'filterProducts'])->name('women.filter.products');
 
 Route::resource('items', ProductController::class);
+
