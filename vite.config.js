45e49403b7c2baf4cbb2190ee7110bbf2c94960e
batish844 <batish844@gmail.com>
2024-11-30 03:45:31 +0200

    Delete vite manifest files blocking production

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/Homepage.js',
+                'resources/js/bootstrap.js',
+                'resources/js/common.js',
+                'resources/js/Contact.js',
+
             ],
             refresh: true,
         }),
