e71578fd668b38022ed7bef9c998645ce11a6a47
batish844 <batish844@gmail.com>
2024-11-30 04:44:19 +0200

    app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,16 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot()
+{
+    if (env('APP_ENV') === 'production') {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+        
+        // Redirect all non-www to www (or vice versa)
+        if (request()->getHost() === 'nexiswear.me') {
+            return redirect()->to('https://www.nexiswear.me' . request()->getRequestUri());
+        }
     }
+}
+
 }
