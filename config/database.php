341ad9c23ed0fc55d44361a1c0b5e119318370b7
batish844 <batish844@gmail.com>
2024-11-30 17:28:46 +0200

    Resolve merge conflict in database configuration for PostgreSQL

--- config/database.php
+++ config/database.php
@@ -83,21 +83,12 @@ return [
         ],
 
         'pgsql' => [
-<<<<<<< HEAD
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-=======
-            'driver'   => 'pgsql',
-            'url'      => env('DATABASE_URL'),
-            'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
->>>>>>> Breeze
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
